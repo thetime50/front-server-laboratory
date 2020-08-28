868e5272ef545b041c6896b5502be76af96b16b0
thetime50 <1004763777@qq.com>
2020-08-28 23:56:37 +0800

    back

--- koa-my/src/page/poplar/graph.vue
+++ koa-my/src/page/poplar/graph.vue
@@ -1,18 +1,27 @@
 <template>
-<div class="component-graph">
+<div class="component-graph" ref="graph">
     graph
 </div>
 </template>
 
 <script>
+// <script lang="ts">
 /* message */
-import {Annotator} from 'poplar-annotation'
+// import {Action, Annotator} from "poplar-annotation";
+// import {LabelCategory} from "poplar-annotation/dist/Store/LabelCategory";
+// import {ConnectionCategory} from "poplar-annotation/dist/Store/ConnectionCategory";
+import {xyy} from './demo.js'
 
 export default {
     name: "graph",
     data () {
         return {
         };
+    },
+    mounted(){
+        // this.$nextTick(()=>{
+        //     // new Annotator(xyy,this.$refs.graph)
+        // })
     },
 }
 </script>
